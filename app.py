1ede5669babc5c5f18575bb002aeaadfc37a33e2
Justin Pinkney <justinpinkney@users.noreply.huggingface.co>
2023-01-19 15:39:09 +0000

    Add duplicate CTA (#1)
    - Add duplicate CTA (26b29d47f3d3dac4390a20bb707cda4e06322ea3)
    
    
    Co-authored-by: Multimodal AI art <multimodalart@users.noreply.huggingface.co>

--- app.py
+++ app.py
@@ -139,6 +139,8 @@ with gr.Blocks(title="Image Mixer", css=".gr-box {border-color: #8136e2}") as de
 
 _Created by [Justin Pinkney](https://www.justinpinkney.com) at [Lambda Labs](https://lambdalabs.com/)_
 
+To skip the queue you can <a href="https://huggingface.co/spaces/lambdalabs/image-mixer-demo?duplicate=true" style="display:inline-block;position: relative;"><img style="margin-top: 0;margin-bottom: 0;margin-left: .25em;" src="https://bit.ly/3gLdBN6"></a>
+
 ### __Provide one or more images to be mixed together by a fine-tuned Stable Diffusion model (see tips and advice below👇).__
 
 ![banner-large.jpeg](https://s3.amazonaws.com/moonup/production/uploads/1674039767068-62bd5f951e22ec84279820e8.jpeg)
